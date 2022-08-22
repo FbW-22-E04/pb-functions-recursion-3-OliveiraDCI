a3d5801c751eaa3c42009c46d6fd09b205ed44ad
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-08-22 15:42:26 +0200

    Exercise concluded.

---
+++ solution.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function reverse(str) {
+  if (str.length === 0) return str;
+  return reverse(str.slice(1)) + str[0];
+}
+const string = "hello world";
+console.log(reverse(string)); // return dlrow olleh
